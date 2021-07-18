ac8d4629be0e87bb37d915573991b60f07484b97
Winda Angelina Utama <wau959@gmail.com>
2021-07-18 12:16:05 +0700

    finish testing

--- app/src/main/java/com/dicoding/todoapp/setting/SettingsActivity.kt
+++ app/src/main/java/com/dicoding/todoapp/setting/SettingsActivity.kt
@@ -2,17 +2,7 @@ package com.dicoding.todoapp.setting
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
-import androidx.appcompat.app.AppCompatDelegate
-import androidx.lifecycle.ViewModelProvider
-import androidx.preference.PreferenceFragmentCompat
-import androidx.preference.SwitchPreference
-import androidx.work.*
 import com.dicoding.todoapp.R
-import com.dicoding.todoapp.ui.ViewModelFactory
-import com.dicoding.todoapp.ui.list.TaskViewModel
-import com.dicoding.todoapp.utils.FunctionLibrary
-import com.dicoding.todoapp.utils.reminder.MyWorker
-import java.util.concurrent.TimeUnit
 
 class SettingsActivity : AppCompatActivity() {
 
